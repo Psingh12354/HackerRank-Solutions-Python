11c47dac3f90014d9815bf8b7a4448ad5c7018a4
Psingh12354 <55645997+Psingh12354@users.noreply.github.com>
2020-05-30 10:14:17 +0530

    Add files via upload
    Here I have done a simple program in HackerRank Python.

---
+++ HTML Parser - Part 2.py
@@ -0,0 +1,19 @@
+from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    def handle_comment(self, data):
+        if '\n' not in data:
+            print('>>> Single-line Comment')
+            print(data)
+        elif '\n' in data:
+            print('>>> Multi-line Comment')
+            print(data)
+    def handle_data(self, data):
+        if data != '\n':
+            print('>>> Data')
+            print(data)
+html = ""       
+for i in range(int(input())):
+    html += input().rstrip()+'\n'
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
